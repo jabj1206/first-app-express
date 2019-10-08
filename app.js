b424eaac0044af9464bb10543254666167c6bd91
alejobetancur12 <alejobetancur12@hotmail.com>
2019-10-08 18:21:40 -0500

    works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+app.use(express.urlencoded());
 app.set("view engine", "pug");
 app.set("views", "views");
 
@@ -27,37 +27,26 @@ var schema = mongoose.Schema({
 // definimos el modelo
 var Visitor = mongoose.model("Visitor", schema);
 
-app.get("/", (req, res) => {
-  let name = req.query.name;
+app.get("/", async (req, res) => {
+  let name = req.query.name || "Anónimo";
 
-  Visitor.findOne({ name: name }, function(err, visitor) {
-    // if (err) return console.error(err);
-    if (visitor) {
-      console.log(visitor);
-      visitor.count++;
-      visitor.save(function(err) {
-        if (err) return console.error(err);
-      });
-    } else {
-      if (name) {
-        Visitor.create({ name: `${name}`, count: 1 }, function(err) {
-          if (err) return console.error(err);
-        });
+  if (name === "Anónimo") {
+    let visitor = new Visitor({ name: name, count: 1 });
+    visitor.save()
+  } else {
+    await Visitor.findOne({ name: name }, (e, visitor) => {
+      if (visitor) {
+        visitor.count++;
+        visitor.save();
       } else {
-        Visitor.create({ name: "Anónimo", count: 1 }, function(err) {
-          if (err) return console.error(err);
-        });
+        let visitor = Visitor({ name: name, count: 1 });
+        visitor.save()
       }
-    }
-  });
+    });
+  }
 
-  Visitor.find(function(err, visitor) {
-    if (err) return console.error(err);
-    console.log(visitor);
-    res.render("index",{visitors: visitor})
-  });
-
-  // res.send("<h1>El visitante fue almacenado con éxito</h1>");
+  const visitors = await Visitor.find();
+  res.render("index", { visitors: visitors });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
