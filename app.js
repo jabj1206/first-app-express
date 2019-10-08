4cddaadff51333ec2f3cf7a9c43520cc8f395844
alejobetancur12 <alejobetancur12@hotmail.com>
2019-10-08 12:45:41 -0500

    ready saludame2

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  if (req.query.nombre) {
-    res.send("<h1>Hola " + req.query.nombre + "!</h1>");
-  }
-  res.send("<h1>Hola desconocido!</h1>");
+app.get("/makers/:param", (req, res) => {
+  let param = req.params.param[0].toUpperCase() + req.params.param.slice(1);
+
+  res.send("<h1>Hola " + param + "!</h1>");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
