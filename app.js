36150146a283e54aca7003c63ebad21f85406af2
alejobetancur12 <alejobetancur12@hotmail.com>
2019-10-08 19:44:05 -0500

    works

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
-mongoose.connection.on("error", function(e){ console.error(e); });
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/users", {
+  useNewUrlParser: true
+});
+mongoose.connection.on("error", function(e) {
+  console.error(e);
+});
 app.use(express.urlencoded());
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
 // definimos el schema
-var schema = mongoose.Schema({ count: Number ,name: String });
+var schema = mongoose.Schema({ name: String, email: String, password: String });
 // definimos el modelo
-var RecurrentVisitor = mongoose.model("RecurrentVisitor", schema);
-app.get('/', async (req, res) => {
-  let visitor
-  let name = req.query.name || 'Anónimo';
-  if(name === 'Anónimo')
-    visitor = new RecurrentVisitor({ name: name, count: 1 });
-  else{
-    visitor = await RecurrentVisitor.findOne({ name: name })
-    if(visitor)
-      visitor.count += 1;
-    else
-      visitor = new RecurrentVisitor({ name: name, count: 1 });
-  }
-  await visitor.save();
-  const visitors = await RecurrentVisitor.find()
-  res.render('visitors', { visitors: visitors })
+var User = mongoose.model("User", schema);
+
+app.get("/register", async (req, res) => {
+  res.render("form");
 });
 
-app.listen(3000, () => console.log('listening 3000'));
+app.post("/register", async (req, res) => {
+  const {name, email, password} = req.body
+  const user = new User({name, email, password });
+  await user.save();
+  res.redirect("/");
+});
+
+app.get("/", async (req, res) => {
+  const users = await User.find();
+  console.log(users);
+  res.render("index", { users: users });
+});
+
+app.listen(3000, () => console.log("listening 3000"));
